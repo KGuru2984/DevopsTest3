a5bb357d18146e9ad69e9a4b78b6c9093f3b2501
KGuru2984 <express2gk@gmail.com>
2024-06-15 18:14:46 +0530

    Committed on 15th Jun 2024 18:14

---
+++ src/main/java/Test1/ClassTest1.java
@@ -0,0 +1,13 @@
+package Test1;
+
+public class ClassTest1 {
+	
+	public static int sum(int a,int b)
+	{
+		return a+b;
+	}
+	public static void main(String[] args) {
+		System.out.println("Hello");
+	}
+
+}
---
+++ src/test/java/Test1/TestingClass1.java
@@ -0,0 +1,14 @@
+package Test1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestingClass1 {
+
+	@Test
+	void test() {
+	
+		 assertEquals(5, ClassTest1.sum(2, 3));
+	}
+}
